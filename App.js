a1b50e161bbfcf43f5a4a5ff7148b4380e84e691
Harsh_V_Singh <harshvardhan707@gmail.com>
2025-04-05 20:52:42 +0530

    episode-2

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 /**
  * 
  * 
@@ -24,7 +26,7 @@ const parent = React.createElement(
     "div", 
     { id: "child" }, 
     [
-        React.createElement("h1", {}, "I am an h1 tag"), 
+        React.createElement("h1", {}, "This is Namaste React"), 
         React.createElement("h2", {}, "I am an h2 tag"),
     ]
   ),
